e83507f28e2ed21a19b5b10ce8c2363579b1479b
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-07-20 12:23:00 -0400

    sum of divisors implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static int sumOfDivisors(int start, int end, int a, int b) {
-        // your code here
-        return 0;
+        IntStream intStream=IntStream.range(start,end+1);
+        int sum=intStream.filter(num -> num%a==0 || num%b==0).sum();
+        return sum;
     }
 
     public static void main(String[] args) {

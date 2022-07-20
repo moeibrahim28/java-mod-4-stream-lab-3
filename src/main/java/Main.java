b975fdc4e85289890611e52b3f8212bc8a512f9d
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-07-20 12:25:31 -0400

    range changed to range closed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,13 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static int sumOfDivisors(int start, int end, int a, int b) {
-        IntStream intStream=IntStream.range(start,end+1);
-        int sum=intStream.filter(num -> num%a==0 || num%b==0).sum();
+        IntStream intStream = IntStream.rangeClosed(start, end);
+        int sum = intStream.filter(num -> num % a == 0 || num % b == 0).sum();
         return sum;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int start = scanner.nextInt();
         int end = scanner.nextInt();
         int a = scanner.nextInt();
